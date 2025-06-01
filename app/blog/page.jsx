4e6a1c9306ccc1fd2e7a1427941ef56dddd8142a
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-06-01 22:53:32 +0100

    working on the portfolio and blog page

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,7 +1,52 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function BlogPage() {
     return (
-        <div>
-            <h1>Welcome to the BLOG page</h1>
+        <div className="flex flex-col gap-5 my-10 items-center">
+            <Link href="/blog/textId" className="flex gap-20">
+                <div className="w-1/3">
+                    <Image src="/images/apps.jpg" alt="app image" height={250} width={400} />
+                </div>
+
+                <div className="w-1/2 flex flex-col gap-4 justify-center">
+                    <h1>Text</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid adipisci debitis amet iure ipsum quas alias quia, voluptas numquam! Molestias voluptas odit neque, quas omnis magnam quaerat nam. Quas, dolorem! Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto nostrum magni ratione facilis consectetur eveniet esse hic nulla suscipit molestias?</p>
+                </div>
+            </Link>
+
+            <Link href="/blog/textId" className="flex gap-20">
+                <div className="w-1/3">
+                    <Image src="/images/people.jpg" alt="app image" height={250} width={400} />
+                </div>
+
+                <div className="w-1/2 flex flex-col gap-4 justify-center">
+                    <h1>Text</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid adipisci debitis amet iure ipsum quas alias quia, voluptas numquam! Molestias voluptas odit neque, quas omnis magnam quaerat nam. Quas, dolorem! Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto nostrum magni ratione facilis consectetur eveniet esse hic nulla suscipit molestias?</p>
+                </div>
+            </Link>
+
+            <Link href="/blog/textId" className="flex gap-20">
+                <div className="w-1/3">
+                    <Image src="/images/apps.jpg" alt="app image" height={250} width={400} />
+                </div>
+
+                <div className="w-1/2 flex flex-col gap-4 justify-center">
+                    <h1>Text</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid adipisci debitis amet iure ipsum quas alias quia, voluptas numquam! Molestias voluptas odit neque, quas omnis magnam quaerat nam. Quas, dolorem! Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto nostrum magni ratione facilis consectetur eveniet esse hic nulla suscipit molestias?</p>
+                </div>
+            </Link>
+
+            <Link href="/blog/textId" className="flex gap-20">
+                <div className="w-1/3">
+                    <Image src="/images/people.jpg" alt="app image" height={250} width={400} />
+                </div>
+
+                <div className="w-1/2 flex flex-col gap-4 justify-center">
+                    <h1>Text</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid adipisci debitis amet iure ipsum quas alias quia, voluptas numquam! Molestias voluptas odit neque, quas omnis magnam quaerat nam. Quas, dolorem! Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto nostrum magni ratione facilis consectetur eveniet esse hic nulla suscipit molestias?</p>
+                </div>
+            </Link>
         </div>
     )
 }
--- app/portfolio/[category]/page.jsx
+++ app/portfolio/[category]/page.jsx
@@ -1,7 +1,50 @@
-export default function Category() {
+"use client"
+
+import Button from "@/components/button/Button"
+import Image from "next/image"
+
+export default function Category({ params }) {
+    console.log(params)
     return (
         <div>
-            <h1>Category</h1>
+            <h1 className="text-3xl font-semibold text-green-600 capitalize">
+                {params.category}
+            </h1>
+            <div className="flex mt-[50px] mb-[100px] gap-20">
+                <div className="w-1/2 flex flex-col gap-4 justify-center">
+                    <h1 className="text-2xl font-semibold">Text</h1>
+                    <p className="text-md">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt voluptatum aliquam veritatis nam possimus, a, accusantium iure dolorem, fugiat in placeat officiis totam voluptates laborum ut est nihil minus consectetur! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, similique. Deserunt voluptate molestiae vitae neque pariatur facere vel, magnam quisquam.</p>
+                    <Button text="See more" url="#"/>
+                </div>
+
+                <div className="w-1/2">
+                    <Image src="/images/people.jpg" alt="website image" className="object-cover" height={500} width={500} />
+                </div>
+            </div>
+
+            <div className="flex mt-[50px] mb-[100px] gap-20">
+                <div className="w-1/2">
+                    <Image src="/images/websites.jpg" alt="website image" className="object-cover" height={500} width={500} />
+                </div>
+
+                <div className="w-1/2 flex flex-col gap-4 justify-center">
+                    <h1 className="text-2xl font-semibold">Text</h1>
+                    <p className="text-md">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt voluptatum aliquam veritatis nam possimus, a, accusantium iure dolorem, fugiat in placeat officiis totam voluptates laborum ut est nihil minus consectetur! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, similique. Deserunt voluptate molestiae vitae neque pariatur facere vel, magnam quisquam.</p>
+                    <Button text="See more" url="#"/>
+                </div>
+            </div>
+
+            <div className="flex mt-[50px] mb-[100px] gap-20">
+                <div className="w-1/2 flex flex-col gap-4 justify-center">
+                    <h1 className="text-2xl font-semibold">Text</h1>
+                    <p className="text-md">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt voluptatum aliquam veritatis nam possimus, a, accusantium iure dolorem, fugiat in placeat officiis totam voluptates laborum ut est nihil minus consectetur! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, similique. Deserunt voluptate molestiae vitae neque pariatur facere vel, magnam quisquam.</p>
+                    <Button text="See more" url="#"/>
+                </div>
+
+                 <div className="w-1/2">
+                    <Image src="/images/illustration.png" alt="website image" className="object-cover" height={400} width={400} />
+                </div>
+            </div>
         </div>
     )
 }
